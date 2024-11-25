affcbf9941bff74baed1ec524bfb49239401c32f
Lara_Aiko <larasagaiif@gmail.com>
2024-11-25 11:40:38 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+    <h1>Hello word</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Homes';
+}
